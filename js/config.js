1e79e361ab7d45c1d8e8959730b7002687758ab0
carolinp <cmpate13@gmail.com>
2017-09-26 12:44:34 -0400

    add config files

---
+++ js/config.js
@@ -0,0 +1,20 @@
+// Add your own configuration options to this file
+// and save it as config.js
+
+var configOptions = {
+    'bingAPI': 'BING API HERE',
+    'attribution': 'Baltimore Sun Graphic, ',
+    'initCoords': [39.2904, -76.6122], // Baltimore
+    'mapzen-api': 'MAPZEN KEY HERE',
+    'columnWidth': 182.38, // L.A. Times width of a print column on six-column page
+    'gutterWidth': 10 // L.A. Times gutter between column
+}
+
+//var configOptions = {
+//    'bingAPI': 'AqEQ_HqipMZe_KnMeHEJ_CEtkNG9Y34_aXaGeIya4fBtc4hTIA9KYzMfFaK5nbK5',
+//    'attribution': '@latimesgraphics, ',
+//    'initCoords': [34.0425, -118.24],
+//    'mapzen-api': 'mapzen-uxhmqQc',
+//    'columnWidth': 110.45,
+//    'gutterWidth': 12
+//}
